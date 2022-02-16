4e889ea5d826e6cc42ea26b108ae57b8cab07e02
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-02-16 20:13:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -49,8 +49,8 @@ xteamkey = 'Dawnfrostkey'
 namaig = 'mursid.st'
 namagithub = 'Botwa021'
 kasihcaption = `Follow Ig @mursid.st`
-namakontak1 = 'Nomer Bot'
-namakontak2 = 'Mursid S'
+namakontak1 = '🐼🔥'
+namakontak2 = 'Mursid (Owner)'
 
 //kasihcaption = `Follow Ig @mursid.st`
 
@@ -74,7 +74,7 @@ global.eror = '_*Server Error kak*_'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
 
-// Ubah saja SC dari AlyaaXzy
+// Ubah saja SC dari MursidXzy
 global.image = 'https://telegra.ph/file/de21f5b2b1f3b52362e3a.jpg'//thumbnail
 
 // tingkat kesulitan, semakin tinggi semakin susah
