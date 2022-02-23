aca9e64ec88922a7c0686303e4badf07b9af0f31
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-02-23 18:45:22 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 const contacts = [
-  ['6288233832771', ' Mursid S'],
-  ['6289612698583', ' 🔥🐼'],
+  ['6288233832771', ' Mursid (Owner)'],
+  ['6289612698583', ' M'],
 ]
 function handler(m) {
   m.reply('Ada perlu apa kak cari owner?, chat *P* tidak akan di balas')
