7099017996af87a0e9d175cd29bcecd1dbb18b10
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-03-17 10:09:06 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-*────────「 SCRIPT 」 ────────*
+*────────「 _*SCRIPT*_ 」 ────────*
 Hai 👋
-saya AlyaaXzy_1
-Bot Ini Menggunakan Sc dari github
+saya MursidXzy
+_*MAU SC INI BELI BANG 10K DOANG CHAT OWNER wa.me/6288233832771*_
 -
-`.trim(), '© AlyaaXzy', 'Thanks', '.tqto')
+`.trim(), '© *_BOTCAHX_*', 'Thanks', '.tqto')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
