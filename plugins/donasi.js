b66f1d3e86fd0aaa8418dd9eada8b78679fcb3e3
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-04-26 13:04:17 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,33 +1,32 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(image + 'donasi')).buffer(), `
-╭═══ 〔 𝐃𝐎𝐍𝐀𝐒𝐈 〕 ══❉
-║│➸ *_Dana_* :
-║│➸ *088233832771*
-║│
-║│➸ *_Pulsa_* :
-║│➸ *089612698583*
-║│➸ *088233832771*
-║│
-║│➸ *_Saweria_* :
-║│➸ *saweria.co/mursid25*
-║│
-║│➸ *_Website_*
-║│➸ *_https://bit.ly/3LeJCXu_*
-║│➸ *_https://bit.ly/3LmtovB_*
-║│
-║│➸ *_Owner Bot_* :
-║│➸ *wa.me/6288233832771*
-║│
-║│➸「 _*Grup Bot 1*_ 」
-║│   https://chat.whatsapp.com/LPFQ2X1cnihB0fb8F8cZau
-║│
-║│➸「 _*Grup Bot 2*_ 」
-║│   https://chat.whatsapp.com/HjRHck1G3WRHOx97fJkdMN
-║│
-║│➸「 _*Grup Bot 3*_ 」
-║│   https://chat.whatsapp.com/HcGcIB09sIvKrGytO8yfFn
-║╰──────────────────
-╰═══════════════════❉
+╭──『 𝐃𝐎𝐍𝐀𝐒𝐈 』──⬣
+│⬡ *_Dana_* :
+│⬡ *088233832771*
+│
+│⬡ *_Pulsa_* :
+│⬡ *089612698583*
+│⬡ *088233832771*
+│
+│⬡ *_Saweria_* :
+│⬡ *saweria.co/mursid25*
+│
+│⬡ *_Website_*
+│⬡ *_https://bit.ly/3LeJCXu_*
+│⬡ *_https://bit.ly/3LmtovB_*
+│
+│⬡ *_Owner Bot_* :
+│⬡ *wa.me/6288233832771*
+│
+│⬡「 _*Grup Bot 1*_ 」
+│   https://chat.whatsapp.com/LPFQ2X1cnihB0fb8F8cZau
+│
+│⬡「 _*Grup Bot 2*_ 」
+│   https://chat.whatsapp.com/HjRHck1G3WRHOx97fJkdMN
+│
+│⬡「 _*Grup Bot 3*_ 」
+│   https://chat.whatsapp.com/HcGcIB09sIvKrGytO8yfFn
+╰──────────────⬣
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
